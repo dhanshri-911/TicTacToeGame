7c39980677b873b67e998cac114281201fc5f657
Dhanshri911 <dszingade911@gmail.com>
2022-03-15 20:30:24 +0530

    UC2 playerLetter and ComputerLetter

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,21 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-
+    static char playerLetter ;
+	static char computerLetter;
+	
+	
+	public static void input(){
+		System.out.println("Enter char ");
+		Scanner sc = new Scanner(System.in);
+		char input = sc.next().charAt(0);
+		if(input == 'X' || input == 'O' ){
+			switch(input){
+				case 'X': 
+					playerLetter = 'X';
+					computerLetter = 'O';
+					break;
+					
+				case 'O':
+					playerLetter= 'O';
+					computerLetter = 'X';
+			}
+		}
+		else
+		System.out.println("input is not correct");	
+	}
 	public static void main(String[] args) {
+		input();
+		}
 		
-    
-    
-    char board[] = new char[10];	
-	
-	
-		for(int i = 0; i < 9; i++) {
-			board[i] = ' ';
-			
-			System.out.println("**Game started**");	
-	}
-    
-	}
+ }
 	
 
-}
