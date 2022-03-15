342102dc2f54f0838e4838e8ee98df44e9f7c82d
Dhanshri911 <dszingade911@gmail.com>
2022-03-15 20:03:41 +0530

    start Game

---
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		
+    
+    
+    char board[] = new char[10];	
+	
+	
+		for(int i = 0; i < 9; i++) {
+			board[i] = ' ';
+			
+			System.out.println("**Game started**");	
+	}
+    
+	}
+	
+
+}
