5203bf051c4b23921159d3a560ea3e99b1eae3b4
Dhanshri911 <dszingade911@gmail.com>
2022-03-20 02:35:27 +0530

    Added:UC3 show board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -2,20 +2,63 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	   static int boardIndex;
-		public static void main(String[] args) {
-			int indexSize = 10;
-		    char[] board = new char[10];
-				int charIndex = 1;
-				for(int i = 1; i <= 3; i++){	
-					for(int j = 1; j <= 3; j++){
-						System.out.print(" "+board[charIndex]+" ");
-						charIndex++;
-						if(j !=  3)
-							System.out.print("|");
-					}
-					System.out.println("");
-		}
-}}
+	final int boardSize = 10;
+	char computerLetter = ' ';
+	char palyerLetter = ' ';
+	char board[] = new char[boardSize];
+	Scanner sc = new Scanner(System.in);
 	
+	TicTacToeGame(){		
+		for(int i = 0; i < boardSize; i++)
+			board[i] = ' ';
+			System.out.println("**Game started**");	
+	}
+	
+	public void inputSymbol(){
+	System.out.println("Enter your symbol X or O :");
+	
+	char input = sc.next().charAt(0);
+		if(input == 'X' || input == 'O' || input == 'x' || input == 'o')
+	{
+	switch(input)
+	{
+	case 'x':
+	case 'X': 
+		palyerLetter = 'X';
+		computerLetter = 'O';
+	case 'o':
+	case 'O':
+		palyerLetter = 'X';
+		computerLetter = 'X';
+	}
+	}
+	else
+		System.out.println("wrong input");	
+	}	
+	
+	public void showBoard(){
+	int charIndex = 0;
+	for(int i = 1; i <= 3; i++){
+	
+		for(int j = 1; j <= 3; j++){
+			System.out.print(" "+board[charIndex]+" ");
+			charIndex++;
+			if(j !=  3)
+			System.out.print("|");
+		}
+		System.out.println("");
 		
+		for(int j = 1; j <= 11 && i != 3; j++)
+			System.out.print("-");
+			System.out.println("");				
+	}
+	System.out.println("");
+	}
+	public static void main(String... args){
+	
+		TicTacToeGame TicTacToe = new TicTacToeGame();			
+		TicTacToe.inputSymbol();
+		TicTacToe.showBoard();
+
+	}
+}
